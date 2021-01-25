23ca832a91eccf80671f44bd7b959e238d8289df
roleqx <wizzsha@gmail.com>
2021-01-26 00:48:11 +0100

    fix: chat recursive re-render and add input length limit

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -17,51 +17,99 @@
       <!-- SECTION CHAT MESSAGES -->
       <div
         id="chat-messages"
+        ref="chatRef"
         class="flex-grow overflow-y-auto flex flex-col-reverse text-sm 2xl:text-base"
       >
         <div
-          v-for="(message, i) in chat.reverse()"
-          :key="i"
+          v-for="message in chat"
+          :key="message.id"
           class="hover:bg-white px-2 py-1"
         >
-          <span class="font-bold text-gray-800">roleqx</span>: {{ i }} -
-          {{ message }}
+          <span class="font-bold text-gray-800">roleqx</span>:
+          {{ message.message }}
         </div>
       </div>
       <!-- SECTION CHAT INPUT  -->
       <div
         id="chat-input"
-        class="h-14 2xl:h-16 px-2 py-2 mt-1 border-t border-gray-500 border-opacity-40 bg-gray-300"
+        class="h-18 2xl:h-18 px-2 pt-2 mt-1 border-t border-gray-500 border-opacity-40 bg-gray-300"
       >
         <!-- INPUT EL  -->
-        <input
-          placeholder="Type your answer..."
-          id="answer-input"
-          class="h-full w-full rounded-t-lg border-b-4 border-teal-400 ring-teal-200 pl-2 focus:border-teal-500 bg-gray-100 focus:bg-white text-gray-800 2xl:text-lg"
-          @keydown.enter="
-            e => {
-              chat.push(e.target.value);
-              e.target.value = '';
-            }
-          "
-        />
+        <form @submit.prevent="answer">
+          <input
+            v-model="inputMessage"
+            placeholder="Type your answer..."
+            maxlength="100"
+            spellcheck="false"
+            autocomplete="off"
+            id="answer-input"
+            class="h-12 2xl:h-12 w-full rounded-t-lg border-b-4 border-teal-400 ring-teal-200 pl-2 focus:border-teal-500 bg-gray-100 focus:bg-white text-gray-800 2xl:text-lg"
+          />
+          <div
+            class="h-4 text-xs text-right text-gray-600"
+            :class="[
+              messageCount <= 100 ? 'text-gray-600' : 'text-red-500 font-bold',
+            ]"
+          >
+            {{ messageCount }}/100
+          </div>
+        </form>
       </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
+
+interface Message {
+  message: string;
+  id: number;
+}
 export default defineComponent({
-  data() {
-    return {
-      chat: [],
+  setup() {
+    const chat = ref<Message[]>([]);
+    const inputMessage = ref("");
+    const messageCount = computed(() =>
+      inputMessage.value.length <= 999 ? inputMessage.value.length : 999
+    );
+
+    const chatRef = ref<HTMLElement | null>(null);
+
+    const scrollChat = () => {
+      // scroll back to the bottom upon sending a new message
+      if (chatRef.value) {
+        const chatEl = chatRef.value;
+        // only scroll if there is a scrollbar
+        if (chatEl.scrollHeight > chatEl.clientHeight) {
+          // only scroll if the scroll bar is not all the way to the bottom upon sending a message
+          if (chatEl.scrollTop != 0) {
+            chatEl.scrollTop = 0;
+          }
+        }
+      }
     };
+    const answer = () => {
+      if (!inputMessage.value) return;
+      // emulate latency
+      setTimeout(() => {
+        chat.value.unshift({ message: inputMessage.value, id: Date.now() });
+        scrollChat();
+        inputMessage.value = "";
+      }, 100);
+    };
+
+    return { chat, inputMessage, messageCount, answer, chatRef };
   },
 });
 </script>
 
 <style scoped>
+#chat-messages {
+  flex: 1 1 auto;
+  height: 0;
+}
+
 #answer-input:focus {
   animation: flashBorder 0.8s ease-in-out infinite;
 }
