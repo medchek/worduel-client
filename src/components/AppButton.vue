5d3538eb117ead64cfed56e9865061e061a89716
roleqx <wizzsha@gmail.com>
2021-01-30 15:13:35 +0100

    Add a button component that includes a loader

---
+++ src/components/AppButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button :disabled="disabled">
+    <!-- @click="begin" -->
+    <loader v-if="loading" size="40" />
+    <span v-else><slot></slot></span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Loader from "./Loader.vue";
+
+export default defineComponent({
+  props: {
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  components: { Loader },
+});
+</script>
