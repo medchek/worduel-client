fc04473436bca1c63312d2c7272990a4811c1bba
roleqx <wizzsha@gmail.com>
2021-05-02 22:24:09 +0100

    chore: expand tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,39 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   // mode: "jit",
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+      textColor: ["active"],
+    },
+  },
   theme: {
     extend: {
       colors: {
         teal: colors.teal,
         bgray: colors.blueGray,
         tgray: colors.trueGray,
+        emerald: colors.emerald,
+        cyan: colors.cyan,
+      },
+      borderWidth: {
+        "6": "6px",
+      },
+      height: {
+        "18": "4.5rem",
+        "9/10": "90%",
+      },
+      width: {
+        "9/10": "90%",
+      },
+      strokeWidth: {
+        "3": "3",
+        "5": "5",
+        "10": "10",
+        "15": "15",
       },
     },
   },
   purge: ["./src/**/*.html", "./src/**/*.vue"],
-  variants: {},
   plugins: [],
 };
